93d52a72bb3056e23091850dd4197e6f4dbd3a80
Matthew Broatch <mnbroatch@gmail.com>
2025-01-11 17:08:46 -0800

    add blurb about looking for work -|:]

--- create-summary-text.js
+++ create-summary-text.js
@@ -38,7 +38,9 @@ ${createTable(plagiarismCase.additional)}
 
 beep boop, I'm a bot -|:] It is this bot's opinion that ${user} should be banned for karma manipulation. Don't feel bad, they are probably a bot too.
 
-Confused? Read the [FAQ](https://www.reddit.com/user/reply-guy-bot/comments/n9fpva/faq/?plagiarist=${plagiarismCase.author}) for info on how I work and why I exist.`
+Confused? Read the [FAQ](https://www.reddit.com/user/reply-guy-bot/comments/n9fpva/faq/?plagiarist=${plagiarismCase.author}) for info on how I work and why I exist.
+
+*My creator is looking for work! If you are hiring a remote or Los Angeles based web developer, send me a message and I'll pass it along -|:]*`
 }
 
 function createTable (plagiarismCases) {
